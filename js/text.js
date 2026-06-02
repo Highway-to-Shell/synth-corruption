a82b5ccfc927a1a290a5d5f2c8d0acd791fb97d4
HawkinWay <1826784860@qq.com>
2026-06-02 14:54:25 +0800

    feat: add UI font and state machine

--- js/text.js
+++ js/text.js
@@ -2,13 +2,16 @@
   'use strict';
 
   const SC = window.SC;
+  const FONT_STACK = '"Press Start 2P", Consolas, Monaco, monospace';
 
   SC.drawText = function drawText(ctx, text, x, y, size, color = SC.colors.white, align = 'center') {
     ctx.save();
-    ctx.font = `${size}px Consolas, Monaco, monospace`;
+    ctx.font = `${size}px ${FONT_STACK}`;
     ctx.textAlign = align;
     ctx.textBaseline = 'top';
     ctx.fillStyle = color;
+    ctx.shadowColor = color;
+    ctx.shadowBlur = Math.max(0, size * 0.18);
     ctx.fillText(String(text), x, y);
     ctx.restore();
   };
