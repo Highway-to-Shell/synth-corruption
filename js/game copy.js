8b63230c1157f3e50f49d3e8a6e852a7746d3322
xiaopianzi <xpz@xxx.com>
2026-06-02 15:27:16 +0800

    feat: add chaser enemies and test checklist

---
+++ js/game copy.js
@@ -0,0 +1,140 @@
+(function () {
+  'use strict';
+
+  const SC = window.SC;
+
+  class Game {
+    constructor(canvas) {
+      this.canvas = canvas;
+      this.ctx = canvas.getContext('2d');
+      this.time = 0;
+      this.states = new SC.StateMachine(SC.STATES.SPLASH);
+      this.player = new SC.Player();
+      this.score = 0;
+      this.highScore = 0;
+      this.bullets = [];
+      this.enemies = [];
+      this.enemyTimer = 1.2;
+      this.spawnLevel = 1;
+    }
+
+    resetRun() {
+      this.score = 0;
+      this.bullets = [];
+      this.enemies = [];
+      this.enemyTimer = 1.2;
+      this.spawnLevel = 1;
+      this.player.reset();
+    }
+
+    startRun() {
+      this.resetRun();
+      this.states.startGame();
+    }
+
+    endRun() {
+      if (!this.states.is(SC.STATES.PLAYING)) return;
+      this.highScore = Math.max(this.highScore, Math.floor(this.score));
+      this.states.endGame();
+    }
+
+    spawnBullet(x, y, angle) {
+      this.bullets.push(new SC.Bullet(x, y, angle));
+    }
+
+    spawnEnemy() {
+      this.enemies.push(SC.createEnemyAtEdge(this, this.spawnLevel));
+    }
+
+    update(dt) {
+      this.time += dt;
+
+      if (SC.input.just.pause) {
+        this.states.togglePause();
+      }
+
+      if (this.states.is(SC.STATES.SPLASH)) {
+        if (SC.input.just.shoot || SC.input.just.start) this.startRun();
+        SC.input.consumeFrame();
+        return;
+      }
+
+      if (this.states.is(SC.STATES.GAMEOVER)) {
+        if (SC.input.just.shoot || SC.input.just.start) this.startRun();
+        SC.input.consumeFrame();
+        return;
+      }
+
+      if (this.states.is(SC.STATES.PAUSED)) {
+        SC.input.consumeFrame();
+        return;
+      }
+
+      if (this.states.is(SC.STATES.PLAYING)) {
+        this.updatePlaying(dt);
+      }
+
+      SC.input.consumeFrame();
+    }
+
+    updatePlaying(dt) {
+      const worldDt = dt * (this.player.warping ? 0.45 : 1);
+      this.score += dt * 100;
+      this.spawnLevel = 1 + Math.floor(this.score / 900);
+
+      this.player.update(dt, this);
+      this.updateBullets(dt);
+      this.updateEnemies(worldDt);
+      this.updateSpawns(worldDt);
+      this.handleCollisions();
+    }
+
+    updateBullets(dt) {
+      for (let i = this.bullets.length - 1; i >= 0; i--) {
+        if (!this.bullets[i].update(dt)) this.bullets.splice(i, 1);
+      }
+    }
+
+    updateEnemies(dt) {
+      for (let i = this.enemies.length - 1; i >= 0; i--) {
+        const enemy = this.enemies[i];
+        enemy.update(dt, this);
+        if (enemy.dead) this.enemies.splice(i, 1);
+      }
+    }
+
+    updateSpawns(dt) {
+      this.enemyTimer -= dt;
+      if (this.enemyTimer > 0) return;
+      this.spawnEnemy();
+      this.enemyTimer = Math.max(0.45, 1.25 - this.spawnLevel * 0.08);
+    }
+
+    handleCollisions() {
+      for (const enemy of this.enemies) {
+        if (Math.hypot(this.player.x - enemy.x, this.player.y - enemy.y) < this.player.radius + enemy.radius * 0.8) {
+          this.endRun();
+          return;
+        }
+      }
+
+      for (let i = this.bullets.length - 1; i >= 0; i--) {
+        const bullet = this.bullets[i];
+        let hit = false;
+
+        for (const enemy of this.enemies) {
+          if (!enemy.dead && Math.hypot(bullet.x - enemy.x, bullet.y - enemy.y) < bullet.radius + enemy.radius) {
+            enemy.hit(this);
+            hit = true;
+            break;
+          }
+        }
+
+        if (hit) this.bullets.splice(i, 1);
+      }
+    }
+  }
+
+  SC.Game = Game;
+})();
+
---
+++ js/render copy.js
@@ -0,0 +1,141 @@
+(function () {
+  'use strict';
+
+  const SC = window.SC;
+
+  function clear(ctx) {
+    ctx.fillStyle = SC.colors.bg;
+    ctx.fillRect(0, 0, SC.W, SC.H);
+  }
+
+  function drawGrid(ctx, time) {
+    ctx.save();
+    ctx.strokeStyle = SC.colors.grid;
+    ctx.lineWidth = 1;
+    for (let x = 0; x <= SC.W; x += SC.GRID) {
+      ctx.beginPath();
+      ctx.moveTo(x + 0.5, 0);
+      ctx.lineTo(x + 0.5, SC.H);
+      ctx.stroke();
+    }
+    for (let y = 0; y <= SC.H; y += SC.GRID) {
+      ctx.beginPath();
+      ctx.moveTo(0, y + 0.5);
+      ctx.lineTo(SC.W, y + 0.5);
+      ctx.stroke();
+    }
+    ctx.strokeStyle = SC.colors.gridStrong;
+    ctx.beginPath();
+    ctx.moveTo(0, SC.H / 2 + Math.sin(time * 2) * 3);
+    ctx.lineTo(SC.W, SC.H / 2 + Math.sin(time * 2) * 3);
+    ctx.stroke();
+    ctx.restore();
+  }
+
+  function drawSplash(ctx, game) {
+    drawGrid(ctx, game.time);
+    SC.drawText(ctx, 'SYNTH CORRUPTION', SC.W / 2, 210, 34, SC.colors.cyan);
+    SC.drawText(ctx, 'SURVIVE THE BROKEN SIGNAL', SC.W / 2, 282, 13, SC.colors.magenta);
+    SC.drawText(ctx, 'CLICK OR PRESS ENTER', SC.W / 2, 372, 13, SC.colors.yellow);
+    SC.drawText(ctx, 'WASD / ARROWS MOVE   MOUSE AIM   LEFT CLICK FIRE', SC.W / 2, 424, 10, SC.colors.white);
+  }
+
+  function drawHud(ctx, game) {
+    SC.drawText(ctx, `SCORE ${Math.floor(game.score)}`, 24, 18, 12, SC.colors.cyan, 'left');
+    SC.drawText(ctx, `BEST ${Math.floor(game.highScore)}`, SC.W - 24, 18, 12, SC.colors.magenta, 'right');
+    SC.drawText(ctx, `LEVEL ${game.spawnLevel}`, SC.W / 2, 18, 12, SC.colors.white);
+  }
+
+  function drawOverlay(ctx, title, subtitle, color) {
+    ctx.save();
+    ctx.fillStyle = SC.colors.panel;
+    ctx.fillRect(0, 0, SC.W, SC.H);
+    SC.drawText(ctx, title, SC.W / 2, 235, 28, color);
+    SC.drawText(ctx, subtitle, SC.W / 2, 315, 12, SC.colors.white);
+    ctx.restore();
+  }
+
+  function drawPlayer(ctx, player) {
+    ctx.save();
+    ctx.translate(player.x, player.y);
+    ctx.rotate(player.angle + Math.PI / 2);
+    ctx.strokeStyle = SC.colors.cyan;
+    ctx.shadowColor = SC.colors.cyan;
+    ctx.shadowBlur = player.warping ? 18 : 10;
+    ctx.lineWidth = 2;
+    SC.drawPolygon(ctx, player.shape, player.radius);
+    ctx.stroke();
+    ctx.restore();
+  }
+
+  function drawBullets(ctx, bullets) {
+    ctx.save();
+    ctx.fillStyle = SC.colors.yellow;
+    ctx.shadowColor = SC.colors.yellow;
+    ctx.shadowBlur = 8;
+    for (const bullet of bullets) {
+      ctx.beginPath();
+      ctx.arc(bullet.x, bullet.y, bullet.radius, 0, Math.PI * 2);
+      ctx.fill();
+    }
+    ctx.restore();
+  }
+
+  function drawEnemies(ctx, enemies) {
+    for (const enemy of enemies) {
+      ctx.save();
+      ctx.translate(enemy.x, enemy.y);
+      ctx.rotate(enemy.angle);
+      ctx.strokeStyle = enemy.flash > 0 ? SC.colors.white : enemy.color;
+      ctx.shadowColor = enemy.color;
+      ctx.shadowBlur = enemy.flash > 0 ? 16 : 8;
+      ctx.lineWidth = enemy.flash > 0 ? 3 : 2;
+      SC.drawPolygon(ctx, enemy.shape, enemy.radius);
+      ctx.stroke();
+      ctx.restore();
+    }
+  }
+
+  function drawCrosshair(ctx) {
+    const input = SC.input;
+    ctx.save();
+    ctx.translate(input.mouse.x, input.mouse.y);
+    ctx.strokeStyle = SC.colors.cyan;
+    ctx.lineWidth = 1.5;
+    ctx.beginPath();
+    ctx.moveTo(-9, 0);
+    ctx.lineTo(9, 0);
+    ctx.moveTo(0, -9);
+    ctx.lineTo(0, 9);
+    ctx.stroke();
+    ctx.restore();
+  }
+
+  function drawWorld(ctx, game) {
+    drawGrid(ctx, game.time);
+    drawBullets(ctx, game.bullets);
+    drawEnemies(ctx, game.enemies);
+    drawPlayer(ctx, game.player);
+    drawHud(ctx, game);
+    drawCrosshair(ctx);
+  }
+
+  SC.render = function render(game) {
+    const ctx = game.ctx;
+    clear(ctx);
+
+    if (game.states.is(SC.STATES.SPLASH)) {
+      drawSplash(ctx, game);
+      return;
+    }
+
+    drawWorld(ctx, game);
+
+    if (game.states.is(SC.STATES.PAUSED)) {
+      drawOverlay(ctx, 'PAUSED', 'PRESS ESC TO RESUME', SC.colors.yellow);
+    } else if (game.states.is(SC.STATES.GAMEOVER)) {
+      drawOverlay(ctx, 'SIGNAL LOST', 'CLICK OR PRESS ENTER TO RESTART', SC.colors.red);
+    }
+  };
+})();
+
